985ec010ff52014368c3a6ddf863ebd6345adaa2
Caleb Hans <caleb.hansolo@gmail.com>
2025-04-02 16:04:39 -0500

    i hate how react works with images
    replace FaceDown in switch statement with corresponding card once added to Asset dir and imported in Card.jsx

--- client/my-react-app/src/Components/Card/Card.jsx
+++ client/my-react-app/src/Components/Card/Card.jsx
@@ -1,15 +1,37 @@
 import './Card.css';
+import Assassin from '../Assets/Assassin.png';
+import Ambassador from '../Assets/Ambassador.png';
+import FaceDown from '../Assets/FaceDown.png';
 
 export default function Card({ role, isRevealed}) {
     // card role and isRevealed should be gotten from props in a higher level component
 
+    // Function to select the card image based on role
+    const selectCard = (role) => {
+        switch (role) {
+            case 'Assassin':
+                return Assassin;
+            case 'Ambassador':
+                return Ambassador;
+            case 'Captain':
+                return FaceDown;
+            case 'Contessa':
+                return FaceDown;
+            case 'Duke':
+                return FaceDown;
+            default:
+                return FaceDown;
+        }
+    };
+
     // determine image path
     const imagePath = isRevealed 
-        ? `../Assets/${role}.png`   // show card face if isRevealed is true
-        : `../../Assets/FaceDown.png`; // show face-down card if isRevealed is false
+        ? selectCard(role)   // show card face if isRevealed is true
+        : FaceDown; // show face-down card if isRevealed is false
     
     console.log(imagePath)
 
+    
 
     return (
         <div className="card-container">
